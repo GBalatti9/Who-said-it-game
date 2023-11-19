52c75fcbd48b971aff623d143d8eb00f9ea629cb
GBalatti9 <gas.balatti@gmail.com>
2023-11-19 14:13:54 -0300

    useAnswers logic

--- src/components/Quotes.jsx
+++ src/components/Quotes.jsx
@@ -1,19 +1,35 @@
+// import { useRef, useState } from "react";
 
+import { useAnswers } from "../hooks/useAnswers"
 
 export const Quotes = ({ data }) => {
-    
+
+    const { inputRefs, correctAnswers, handleChange, checkAnswers } = useAnswers();
+
     return (
         <form action="">
             {
-                data.map((p, i) => (
-                    <div key={p + i}>
-                        <p className="mt-2 fw-semibold">{p.quote}</p>
-                        <input type="text" />
+                data.map((q, i) => (
+                    <div key={q + i} className="input-container">
+                        <p className="mt-2 fw-semibold">{q.quote}</p>
+                        <input
+                            className="form-control"
+                            type="text"
+                            name="answer"
+                            id="answer"
+                            placeholder="Who said it?"
+                            ref={(el) => (inputRefs.current[i] = el)}
+                            onChange={(e) => handleChange(e, q.author, i)} />
+                        {
+                            correctAnswers.includes(q.author) && (
+                                <span className="text-danger">{q.author}</span>
+                            )
+                        }
                     </div>
                 ))
             }
             <div className="d-flex justify-content-center">
-                <button className="btn btn-primary m-2 col-4">Verify</button>
+                <button className="btn btn-primary m-2 col-4" onClick={checkAnswers}>Verify</button>
             </div>
         </form>
     )
--- src/hooks/useAnswers.js
+++ src/hooks/useAnswers.js
@@ -1,5 +1,4 @@
-import { useRef, useState } from "react"
-
+import { useRef, useState } from "react";
 
 export const useAnswers = () => {
 
@@ -7,8 +6,46 @@ export const useAnswers = () => {
         userAnswer: '',
         author: '',
         indexInputRef: '',
-    }]);
+    }])
     const [correctAnswers, setCorrectAnswers] = useState([]);
-    const inputsRef = useRef([]);
-    return {}
+    const inputRefs = useRef([]);
+
+    const handleChange = ({ target }, author, i) => {
+        const { value } = target;
+        console.log({ value, author });
+
+        setUserAnswers((prevAnswers) => [
+            ...prevAnswers.slice(0, i),
+            {
+                userAnswer: value,
+                author: author,
+                indexInputRef: i,
+            },
+            ...prevAnswers.slice(i + 1),
+        ]);
+
+    }
+
+    const checkAnswers = (e) => {
+        e.preventDefault();
+        let correctAnswersArray = [];
+        userAnswers.map((answer) => {
+            if (answer.userAnswer.toLowerCase() === answer.author.toLowerCase()) {
+                inputRefs.current[answer.indexInputRef].style.border = '1px solid green';
+            } else {
+                inputRefs.current[answer.indexInputRef].style.border = '1px solid red';
+                correctAnswersArray.push(answer.author);
+            }
+        })
+        console.log({ correctAnswersArray });
+        setCorrectAnswers(correctAnswersArray);
+    }
+
+    return {
+        inputRefs,
+        correctAnswers,
+        handleChange,
+        checkAnswers
+    }
+
 }
